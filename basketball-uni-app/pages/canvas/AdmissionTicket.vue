ad58ec3186069739a3696f2b0a549ac3645de137
殷万波 <740358949@qq.com>
2021-03-02 18:15:14 +0800

    服务表格

---
+++ basketball-uni-app/pages/canvas/AdmissionTicket.vue
@@ -0,0 +1,48 @@
+<template>
+	<view style="background-color: #fff;height: 100%;">
+		<canvas class="hideCanvas" style="border:1px solid #1e1e1e;width: 1500rpx;height: 2000rpx;" id="ticket_id" canvas-id="ticket_id" ></canvas>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				ctx: null
+			}
+		},
+		methods: {
+			async drawTicket() {
+				this.ctx.setFillStyle('#FFFFFF')
+				this.ctx.fillRect(0, 0, 190 * 2, 79 * 2)
+				let ticketTmpPath = await this.downLoadFile('http://39.101.161.231/images/ticket.png')
+				this.ctx.drawImage(ticketTmpPath, 0, 0, 190 * 2, 79 * 2)
+				this.ctx.draw()
+			},
+			// 下载文件到临时路径
+			downLoadFile(imgUrl) {
+				return new Promise((resolve, reject) => {
+					uni.downloadFile({
+					    url: imgUrl, //仅为示例，并非真实的资源
+						success: (res) => {
+							if (res && res.tempFilePath){
+								resolve(res.tempFilePath);
+							}else{
+								reject('not find tempFilePath');
+							}
+						}
+					    
+					});
+				});
+			},
+		},
+		mounted(){
+			this.ctx = uni.createCanvasContext('ticket_id', this)
+			this.drawTicket()
+		},
+	}
+</script>
+
+<style>
+
+</style>
--- basketball-uni-app/pages/introduce/PlayerRank/index.vue
+++ basketball-uni-app/pages/introduce/PlayerRank/index.vue
@@ -1,29 +1,20 @@
 <template>
 	<view class="wrap">
-<<<<<<< HEAD
-		<view class="matte">
+		<!-- <view class="matte"> -->
 			<view class="left_area">
-				<view class="">
-					<image :src="tableData[0].avator" mode=""></image>
-					<span>tableData[0].nickName</span>
-				</view>
+					<image style="width: 30%;height: 30%;" :src="tableData[0].avator" mode=""></image>
+					<span>{{tableData[0].nickName}}</span>
 			</view>
 			<view class="right_area">
-				
+				<ul style="height: 100%;">
+					<li class="rank-item" v-for="(item, index) in goldList">
+						<view class="rank-item-content">
+							<image style="width: 80rpx;height: 80rpx;transform: scale(1);" :src="item.medalUrl" mode=""></image>
+						</view>
+					</li>
+				</ul>
 			</view>
-=======
-		<view class="left_area">
-			<!-- <view class=""> -->
-				<image style="width: 30%;height: 30%;" :src="tableData[0].avator" mode=""></image>
-				<span>{{tableData[0].nickName}}</span>
-			<!-- </view> -->
-		</view>
-		<view class="right_area">
-			<ul>
-				<li class="rank-item"></li>
-			</ul>
->>>>>>> 6e83e5b06ec8a60918b584646ea152b8027d32e5
-		</view>
+		<!-- </view> -->
 		<!-- <u-row >
 			<u-col span="3">
 				<view class="flex-center demo-layout bg-purple bg-purple-dark">排名</view>
@@ -60,7 +51,13 @@
 	export default {
 		data() {
 			return {
-				tableData: []
+				tableData: [],
+				goldList: [
+					{nickName: '1111', score: 432, rank: 1, medalUrl: '/static/imgs/rank/champion200.png'},
+					{nickName: '1111', score: 286, rank: 2, medalUrl: '/static/imgs/rank/second200.png'},
+					{nickName: '1111', score: 256, rank: 3, medalUrl: '/static/imgs/rank/third200.png'},
+					// {nickName: '1111', score: 156, rank: 4, medalUrl: '../../../static/imgs/rank/4 5.png'},
+				]
 			}
 		},
 		methods: {
@@ -68,6 +65,7 @@
 				http.get('weapp/player/getPlayerRank').then(res => {
 					if(res.data.code === 0) {
 						this.tableData = res.data.data
+						// this.goldList = this.tableData.split(0, 5)
 					}
 				})
 			}
@@ -86,50 +84,55 @@
 }
 .wrap{
 	// text-align: center;
+	margin-top: 28rpx;
 	width: '100%';
 	height: 275rpx;
 	display: flex;
-<<<<<<< HEAD
-	background: #f57463;
 	position:relative;
-	.matte{
-		position: absolute;
-		top: 0;
-		right: 0;
-		left: 0;
-		bottom: 0;
-		background: hsla(0, 0%, 100%, .3);
+	padding: 12rpx;
+	// background-image: linear-gradient(#e69874, #e5c471); 
+	// background: hsla(0, 0%, 100%, 0);
+	background-color: #FFFFFF;
+	// box-shadow: 
+				// 0 0 0 1px hsla(0,0%,100%,.3) inset,
+	            // 0 4rpx 12rpx rgba(0, 0, 0, 0.6);
+		// &::before{
+		// 	content: '';
+		// 	position: absolute;
+		// 	top: 0;
+		// 	right: 0;
+		// 	left: 0;
+		// 	bottom: 0;
+		// 	background: hsla(0, 0%, 100%, .6);
+		// 	border-radius: 26rpx;
+		// 	filter: blur(8px);
+		// 	overflow: hidden
+		// }
 		.left_area{
 			height: 100%;
 			width: 50%;
 			padding: 10rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-right: 2rpx solid #efe9e8;
 		}
 		.right_area{
 			height: 100%;
 			width: 50%;
-=======
-	margin-top: 24rpx;
-	border: 2rpx solid #f57463;
-	// background: rgba($color: #646464, $alpha: 0.6);
-	.left_area{
-		width: 50%;
-		padding: 10rpx;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.right_area{
-		width: 50%;
-		padding: 12rpx;
-		.rank-item{
-			height: 58rpx;
-			width: 100%;
-			margin-bottom: 8rpx;
-			background-color: #e5e5e5;
->>>>>>> 6e83e5b06ec8a60918b584646ea152b8027d32e5
+			.rank-item{
+				padding: 8rpx;
+				height: 33%;
+				width: 100%;
+				margin-bottom: 1%;
+				background-color: #efe9e8;
+				.rank-item-content{
+					height: 100%;
+					display: flex;
+					align-items: center
+				}
+			}
 		}
-	}
-	
 }
 .demo-layout {
 	height: 40rpx;
